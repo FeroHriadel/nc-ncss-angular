97ebd8ff7770a3616833496111aa25d5422d4685
FeroHriadel <ferdinand.hriadel@gmail.com>
2026-05-17 22:56:52 +0200

    left-nav hide/show

--- src/app/ncss/navs/left-nav/left-nav.component.ts
+++ src/app/ncss/navs/left-nav/left-nav.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, AfterViewInit } from "@angular/core";
 import { RouterLink } from '@angular/router';
 import { CloseButton } from '../../buttons/close-button/close-button.component';
 import { Collapsible } from '../../popups/collapsible/collapsible.component';
+import { SquareButton } from '../../buttons/square-button/square-button.component';
+import { ChevronDownIcon } from '../../icons/chevron-down.icon';
 
 
 
@@ -18,18 +20,46 @@ interface NavLink {
   standalone: true,
   templateUrl: './left-nav.component.html',
   styleUrl: './left-nav.component.css',
-  imports: [CloseButton, RouterLink, Collapsible]
+  imports: [CloseButton, RouterLink, Collapsible, SquareButton, ChevronDownIcon]
 })
 
 
 
-export class LeftNav {  
+export class LeftNav implements AfterViewInit {  
     @Input() class: string = '';
     @Input() style: {[key: string]: string} = {}
     @Input() id: string = 'nc-left-nav';
     @Input() top: string = '0px';
     @Input() width: string = '';
     @Input() links: NavLink[] = [];
+    
+    isOpen: boolean = true;
+
+    ngAfterViewInit() {
+        const wrapper = document.getElementById('left-nav-app-wrapper');
+        if (wrapper) {
+            const navWidth = this.width || '260px';
+            wrapper.style.marginLeft = navWidth;
+            wrapper.style.transition = 'margin-left 0.3s ease';
+        }
+    }
+
+    close() {
+        this.isOpen = false;
+        const wrapper = document.getElementById('left-nav-app-wrapper');
+        if (wrapper) {
+            wrapper.style.marginLeft = '0';
+        }
+    }
+
+    open() {
+        this.isOpen = true;
+        const wrapper = document.getElementById('left-nav-app-wrapper');
+        if (wrapper) {
+            const navWidth = this.width || '260px';
+            wrapper.style.marginLeft = navWidth;
+        }
+    }
 }
 
 
